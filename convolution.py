fe01b7eddca22549b09f356c56fc8c98af72f993
sabribarac <s.barac@umail.leidenuniv.nl>
2023-01-04 17:50:14 +0100

    Update convolution.py

--- convolution.py
+++ convolution.py
@@ -9,11 +9,12 @@ def apply_convolution(img:np.array, kernel:np.array):
     # Get the height, width, and number of channels of the kernel
     kernel_height,kernel_width = kernel.shape[0],kernel.shape[1]
     
-    # Create a new image of the size minus the border 
+    # Create a new image of original img size minus the border 
     # where the convolution can't be applied
     new_img = np.zeros((height-kernel_height+1,width-kernel_width+1,3)) 
     
     # Loop through each pixel in the image
+    # But skip the outer edges of the image
     for i in range(kernel_height//2, height-kernel_height//2-1):
         for j in range(kernel_width//2, width-kernel_width//2-1):
             # Extract a window of pixels around the current pixel
@@ -26,20 +27,29 @@ def apply_convolution(img:np.array, kernel:np.array):
       
     # Clip values to the range 0-255
     new_img = np.clip(new_img, 0, 255)
-    
-    # Create a PIL image from the new image and display it
-    sImg = Image.fromarray(new_img.astype(np.uint8))
-    sImg.show()
+    return new_img.astype(np.uint8)
 
 if __name__ == "__main__":
-    # Kernel to blur image
-    kernel = np.array([[1,2,1],[2,4,2],[1,2,1]])*1/16
+
+    # kernel for edge detection
+    kernel = np.array([[-1,-1,-1], [-1,8,-1], [-1,-1,-1]])
     
-    # kernel to get the edges of an image
-    # kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-    
-    # Import image through PIL and convert to numpy array
-    img = Image.open('city.jpg')
+    # kernel for vertical edge detection
+    #kernel = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+
+    # kernel for horizontal edge detection
+    # kernel = np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+
+    # Kernel for box blur 
+    # kernel = np.array([[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]])
+
+    # Open the image and convert it to an array
+    # Try to put your own picture!
+    img = Image.open('kitten3.jpg')
     or_img = np.asarray(img)
-  
-    apply_convolution(or_img, kernel)    
+    
+    new_img = apply_convolution(or_img, kernel)
+
+    # Create a PIL image from the new image and display it     
+    sImg = Image.fromarray(new_img)
+    sImg.show() 
